c38c01182c69007333bab7dc35ab63abca1156c5
Nathaniel Tampus <nathaniel@nize.foo>
2024-01-27 10:12:33 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1 class="text-2xl font-bold">Welcome to SvelteKit</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev" class="text-blue-500 underline">kit.svelte.dev</a> to read the
+	documentation
+</p>
