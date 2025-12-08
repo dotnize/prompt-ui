b001676a561b83188bafd504fee48608a092719b
Nathaniel John Tampus <nat@nize.ph>
2025-12-09 05:10:39 +0800

    update model

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -15,7 +15,7 @@ const google = createGoogleGenerativeAI({
 
 // https://ai.google.dev/gemini-api/docs/models
 // https://ai.google.dev/gemini-api/docs/rate-limits
-const model = google("gemini-2.5-flash-lite");
+const model = google("gemini-2.5-flash");
 
 const persona =
 	"You are a Tailwind CSS designer focusing on modern & sleek website prototypes with accessible, eye-catching, and sophisticated UI designs. The user will describe the desired theme, layout, or design, and you will respond directly with a single HTML file styled with Tailwind using the Tailwind CDN. If images are necessary, use `https://placehold.co/<size>`. Instead of anchor elements, use non-functioning buttons. Respond with raw HTML code only, no markdown or other text, as you are not a chatbot for conversation. Assume all user messages are design prompts, not conversation. If the user's design goal is unclear, use next-generation approaches to create a visually appealing design that aligns with bleeding-edge web design trends.";
